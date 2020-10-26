4357e3f388a920da3afb95254958bf78240ced12
wayupthere <mm2oleary@gmail.com>
2020-10-26 04:47:13 -0400

    Add How to start instructions

--- src/graphql/schema/types/QueryType.js
+++ src/graphql/schema/types/QueryType.js
@@ -9,17 +9,8 @@ const QueryType = new GraphQLObjectType({
             args: {
                 url: { type: GraphQLString },
             },
-            //resolve: (root, args) => axios.get(args.url || 'http://swapi.dev/api/people/').then(res => res.data)
-            // resolve: person => person.homeworld ? fetch(person.homeworld).then(response => response.json()).then(json => json.data) : null
             resolve: (root, args) => fetch(args.url || 'http://swapi.dev/api/people/').then(res => res.json())
         }
-        /*person: {
-            type: PersonType,
-            args: {
-                id: { type: GraphQLString },
-            },
-            resolve: (root, args) => fetch(`http://swapi.dev/api/people/${args.id}/`).then(res =>  res.json())// Fetch the person with ID `args.id`,
-        }*/
     }),
 });
 
